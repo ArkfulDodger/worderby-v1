9b069440559fbd7faf9e2bdf4a583a54eab862b4
Noah Reece <noah.w.reece@gmail.com>
2022-05-27 14:29:15 -0400

    back end action cable setup (from dev.to article)

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,23 @@
+class MessagesController < ApplicationController
+  # GET /messages
+  def index
+    messages = Message.all
+    render json: messages
+  end
+
+  def create
+    message = Message.new(message_params)
+    if message.save
+      ActionCable.server.broadcast 'messages_channel', message
+      head :ok
+    else
+      head :ok
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_26_175600) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_27_175010) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_26_175600) do
     t.integer "player1_score"
     t.integer "player2_score"
     t.boolean "is_single_player"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
