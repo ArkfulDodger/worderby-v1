050c560a17f6f0c3275d95fc105022bf20a634f3
Noah Reece <noah.w.reece@gmail.com>
2022-05-31 16:00:12 -0400

    updated URL to deployed heroku

--- client/src/hooks/useURL.js
+++ client/src/hooks/useURL.js
@@ -3,14 +3,17 @@ import { Platform } from "react-native";
 import DeviceInfo from "react-native-device-info";
 
 function useURL(isEmulator = true) {
-  // console.log("ISEMULATOR:", isEmulator);
-  if (isEmulator) {
-    return Platform.OS === "ios"
-      ? "http://localhost:3000"
-      : "http://10.0.2.2:3000";
-  } else {
-    return "http://localhost:3000";
-  }
+  return "https://worderby.herokuapp.com/";
+
+  //#region Local Host Solution
+  // if (isEmulator) {
+  //   return Platform.OS === "ios"
+  //     ? "http://localhost:3000"
+  //     : "http://10.0.2.2:3000";
+  // } else {
+  //   return "http://localhost:3000";
+  // }
+  //#endregion
 }
 
 export default useURL;
