20ef626c37154d1bd2af2006469d93fb0c17120c
Noah Reece <noah.w.reece@gmail.com>
2022-06-01 11:19:14 -0400

    changed url back to local

--- client/src/hooks/useURL.js
+++ client/src/hooks/useURL.js
@@ -3,16 +3,16 @@ import { Platform } from "react-native";
 import DeviceInfo from "react-native-device-info";
 
 function useURL(isEmulator = true) {
-  return "https://worderby.herokuapp.com/";
+  // return "https://worderby.herokuapp.com/";
 
   //#region Local Host Solution
-  // if (isEmulator) {
-  //   return Platform.OS === "ios"
-  //     ? "http://localhost:3000"
-  //     : "http://10.0.2.2:3000";
-  // } else {
-  //   return "http://localhost:3000";
-  // }
+  if (isEmulator) {
+    return Platform.OS === "ios"
+      ? "http://localhost:3000"
+      : "http://10.0.2.2:3000";
+  } else {
+    return "http://localhost:3000";
+  }
   //#endregion
 }
 
