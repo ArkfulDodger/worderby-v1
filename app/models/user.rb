d15193c2bdf1c27cefa02135e389cb00fa556171
Noah Reece <noah.w.reece@gmail.com>
2022-06-01 23:45:42 -0400

    potential bug fix where server crashes if user has no games

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,24 +13,28 @@ class User < ApplicationRecord
     gameobjs =
       Game.where(player1_id: id, is_over: false) +
         Game.where(player2_id: id, is_over: false)
-    games =
-      gameobjs.map do |game|
-        {
-          id: game.id,
-          player1: game.player1,
-          player2: game.player2,
-          is_over: game.is_over,
-          round: game.round,
-          turn: game.turn,
-          player1_score: game.player1_score,
-          player2_score: game.player2_score,
-          is_single_player: game.is_single_player,
-          is_word_played_this_turn: game.is_word_played_this_turn,
-          is_accepted: game.is_accepted,
-          challenger_id: game.challenger_id,
-          challengee_id: game.challengee_id
-        }
-      end
+    if gameobjs
+      games =
+        gameobjs.map do |game|
+          {
+            id: game.id,
+            player1: game.player1,
+            player2: game.player2,
+            is_over: game.is_over,
+            round: game.round,
+            turn: game.turn,
+            player1_score: game.player1_score,
+            player2_score: game.player2_score,
+            is_single_player: game.is_single_player,
+            is_word_played_this_turn: game.is_word_played_this_turn,
+            is_accepted: game.is_accepted,
+            challenger_id: game.challenger_id,
+            challengee_id: game.challengee_id
+          }
+        end
+    else
+      games = []
+    end
   end
 
   def highest_scoring_word
